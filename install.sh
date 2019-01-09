5cc23f3b6ae2826c66dde9b159f697c9a1c6f05f
Julian Inwood <jinw@protonmail.com>
2019-01-09 21:35:41 +0000

    set timezone

--- install.sh
+++ install.sh
@@ -6,3 +6,17 @@ echo "! 66 is the keycode of Caps_Lock
 clear    Lock
 keycode  66 = Escape" > ~/.Xmodmap
 xmodmap ~/.Xmodmap
+
+#download backgroud image
+curl https://cache.desktopnexus.com/cropped-wallpapers/2346/2346173-1920x1080-[DesktopNexus.com].jpg?st=RfAXWSisCuS-cSgAjyuguQ&e=1547072492 > ~/Pictures/wallpaper.jpg
+
+if !  [ -x "$(command -v pacman)"]; then
+    echo 'pacman not found!'
+    exit 1
+fi
+
+sudo pacman -S feh
+#set uk timezone
+sudo pacman -S ntp
+sudo timedatectl set-ntp true
+sudo timedatectl set-timezone Europe/London
