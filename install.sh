f54966baaa97644054f73362299b9e5b6603066c
Julian Inwood <jinw@protonmail.com>
2019-03-05 22:01:55 +0000

    update

--- install.sh
+++ install.sh
@@ -10,22 +10,9 @@ xmodmap ~/.Xmodmap
 #download backgroud image
 curl https://cache.desktopnexus.com/cropped-wallpapers/2346/2346173-1920x1080-[DesktopNexus.com].jpg?st=RfAXWSisCuS-cSgAjyuguQ&e=1547072492 > ~/Pictures/wallpaper.jpg
 
-if !  [ -x "$(command -v pacman)"]; then
-    echo 'pacman not found!'
-    exit 1
-fi
-
-sudo pacman -S feh
 #set uk timezone
-sudo pacman -S ntp
 sudo timedatectl set-ntp true
 sudo timedatectl set-timezone Europe/London
 
-#this should only be ran on thinkpads //todo
-sudo pacman -S tlp tlp-rdw 
-sudo pacman -S tp_smapi linux419-acpi_call 
-systemctl enable tlp.service
-systemctl enable tlp-sleep.service
-systemctl mask systemd-rfkill.service
-systemctl mask systemd-rfkill.socket
-
+ln -sf ./i3/config ~/.config/i3/config
+#more
