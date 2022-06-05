93826ad2ce1d252eb771659e625c15f0abf2dd9f
Julian Inwood <julian@Julians-MBP.lan>
2022-06-05 21:51:41 +0100

    updates

--- os/macos/install.sh
+++ os/macos/install.sh
@@ -3,13 +3,15 @@
 if [[ ! -x "$(command -v brew)" ]]; then
   echo "Installing homebrew...\\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  else
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/julian/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
   echo "Homebrew is already installed.\\n"
 fi
 
 echo "Installing packages"
 echo $(pwd)
-brew bundle --file="./Brewfile"
+brew bundle --file ~/repos/jinwood/dotfiles/os/macos/Brewfile
 
 echo "--------------------------------"
 echo "Configuring OS"
